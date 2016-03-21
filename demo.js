a93c89ef851da0c87fffd469670de2118e3ce9c3
Jan CW Kroeze <thejcwk@gmail.com>
2016-03-21 14:45:17 +0200

    Reformat demo code.

--- demo.js
+++ demo.js
@@ -13,14 +13,13 @@ var rotation = 0;
 var vertexShader;
 var fragmentShader;
 
-function load(path)
-{
+function load(path) {
     var src;
 
     $.ajax({
         async: false,
         url: path,
-        success: function(result) {
+        success: function (result) {
             src = result;
         }
     });
@@ -28,8 +27,7 @@ function load(path)
     return src;
 }
 
-function display()
-{
+function display() {
     var yAxis = vec3.fromValues(0, 1, 0);
     var worldMatrix = mat4.create();
     mat4.rotate(worldMatrix, worldMatrix, rotation, yAxis);
@@ -51,47 +49,39 @@ function display()
     gl.drawArrays(gl.LINES, 0, vertexCount);
 }
 
-function findAttribute(name)
-{
+function findAttribute(name) {
     var loc = gl.getAttribLocation(program, name);
 
-    if (loc == -1)
-    {
+    if (loc == -1) {
         console.error("Could not find attribute '" + name + "'.");
     }
 
     return loc;
 }
 
-function findUniform(name, value)
-{
+function findUniform(name, value) {
     var loc = gl.getUniformLocation(program, name);
 
-    if (loc == null)
-    {
+    if (loc == null) {
         console.error("Could not find uniform '" + name + "'.");
     }
 
     return loc;
 }
 
-function get_web_gl_context()
-{
+function get_web_gl_context() {
     var canvas = document.getElementById("canvas");
-    if (canvas == null)
-    {
+    if (canvas == null) {
         alert("Could not find canvas.");
         return;
     }
 
     var gl = null;
     var names = ["webgl", "experimental-webgl", "webkit-3d", "moz-webgl"];
-    for (var i = 0; i < names.length; i++)
-    {
+    for (var i = 0; i < names.length; i++) {
         gl = canvas.getContext(names[i]);
 
-        if (gl)
-        {
+        if (gl) {
             return gl;
         }
     }
@@ -99,28 +89,24 @@ function get_web_gl_context()
     return null;
 }
 
-function update()
-{
+function update() {
     display();
 
     rotation += rotationDelta;
 }
 
-function init()
-{
+function init() {
     $(window).resize(reshape);
     $(window).keypress(keyboard);
 
     window.setInterval(update, 16);
 }
 
-function keyboard(ev)
-{
+function keyboard(ev) {
     update();
 }
 
- function init_geometry()
- {
+function init_geometry() {
     vertexCount = 0;
     var vertices = [];
     var positionStack = [];
@@ -137,10 +123,8 @@ function keyboard(ev)
     var negativeZRotation = mat3.create();
     positiveZRotation = mat3.rotate(positiveZRotation, positiveZRotation, angle, zAxis);
     negativeZRotation = mat3.rotate(negativeZRotation, negativeZRotation, -angle, zAxis);
-    for (var i = 0; i < lsystem.string.length; i++)
-    {
-        if (lsystem.string.charAt(i) == "F")
-        {
+    for (var i = 0; i < lsystem.string.length; i++) {
+        if (lsystem.string.charAt(i) == "F") {
             vertices = vertices.concat([position[0], position[1], position[2]]);
             vec3.add(position, position, direction);
             vertices = vertices.concat([position[0], position[1], position[2]]);
@@ -148,25 +132,21 @@ function keyboard(ev)
 
             vertexCount += 2;
         }
-        else if (lsystem.string.charAt(i) == "+")
-        {
+        else if (lsystem.string.charAt(i) == "+") {
             vec3.transformMat3(direction, direction, positiveZRotation);
             vec3.normalize(direction, direction);
             vec3.scale(direction, direction, distance);
         }
-        else if (lsystem.string.charAt(i) == "-")
-        {
+        else if (lsystem.string.charAt(i) == "-") {
             vec3.transformMat3(direction, direction, negativeZRotation);
             vec3.normalize(direction, direction);
             vec3.scale(direction, direction, distance);
         }
-        else if (lsystem.string.charAt(i) == "[")
-        {
+        else if (lsystem.string.charAt(i) == "[") {
             positionStack.push(vec3.clone(position));
             directionStack.push(vec3.clone(direction));
         }
-        else if (lsystem.string.charAt(i) == "]")
-        {
+        else if (lsystem.string.charAt(i) == "]") {
             position = positionStack.pop();
             direction = directionStack.pop();
         }
@@ -183,26 +163,23 @@ function keyboard(ev)
     reshape();
 }
 
- /* Taken from http://learningwebgl.com/blog/?p=28 */
- function init_shader(src, type)
- {
+/* Taken from http://learningwebgl.com/blog/?p=28 */
+function init_shader(src, type) {
     var shader;
     shader = gl.createShader(type);
 
     gl.shaderSource(shader, src);
     gl.compileShader(shader);
 
-    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS))
-    {
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
         console.log(gl.getShaderInfoLog(shader));
         return null;
     }
 
     return shader;
- }
+}
 
- function init_shader_program()
- {
+function init_shader_program() {
     var vertexShader = load("/media/l-system/vertex.glsl");
     var vs = init_shader(vertexShader, gl.VERTEX_SHADER);
     var fragmentShader = load("/media/l-system/fragment.glsl");
@@ -213,21 +190,18 @@ function keyboard(ev)
     gl.attachShader(program, fs);
     gl.linkProgram(program);
 
-    if (!gl.getProgramParameter(program, gl.LINK_STATUS))
-    {
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
         console.log(gl.getProgramInfoLog(program));
     }
 
     gl.useProgram(program);
 }
 
-function init_web_gl()
-{
+function init_web_gl() {
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
 }
 
-function reshape()
-{
+function reshape() {
     /* This gets the CSS width and height. */
     var newWidth = $("canvas").width();
     var newHeight = $("canvas").height();
@@ -243,7 +217,7 @@ function reshape()
     console.log("minX = " + minX + ", maxX = " + maxX);
 
     /* This, however, sets the values for the actual HTML element to be
-    the same as the CSS. */
+     the same as the CSS. */
     var canvas = $("canvas");
     canvas.attr("width", newWidth);
     canvas.attr("height", newHeight);
@@ -259,8 +233,7 @@ function reshape()
     display();
 }
 
-function init_lsystem()
-{
+function init_lsystem() {
     lsystem = new LSystem();
     lsystem.setAxiom("F");
 
@@ -274,8 +247,7 @@ function init_lsystem()
     lsystem.step();
 }
 
-function main()
-{
+function main() {
     gl = get_web_gl_context();
 
     init();
@@ -286,10 +258,10 @@ function main()
     reshape();
 }
 
-$(document).ready(function(){
-    $.getScript("/media/l-system/gl-matrix.js", function(){
+$(document).ready(function () {
+    $.getScript("/media/l-system/gl-matrix.js", function () {
         console.log("gl-matrix.js loaded");
-        $.getScript("/media/l-system/lsystem.js", function(){
+        $.getScript("/media/l-system/lsystem.js", function () {
             console.log("lsystem.js loaded");
             main();
         });
